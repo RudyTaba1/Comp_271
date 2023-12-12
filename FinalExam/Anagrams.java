e21e108d272c94b2abb432d66b7555b543d7d36a
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2023-12-11 22:48:19 -0600

    Final Exam DS1!

---
+++ FinalExam/Anagrams.java
@@ -0,0 +1,19 @@
+package FinalExam;
+
+public class Anagrams {
+
+  /**
+   * Determine if two strings are anagrams of each other. Assume that
+   * the strings contain only valid letters. Letter-case is not important,
+   * for example, "BAT" and "Tab" are still anagrams.
+   *
+   * @param first  String one of the two strings to compare
+   * @param second String the other of the two strings to compare
+   * @return true if first and second strings are anagrams of eachother,
+   *         false otherwise or if either string is null
+   */
+  public static boolean areAnagrams(String first, String second) {
+    boolean theyAre = true;
+    return theyAre;
+  } // method areAnagrams
+}
---
+++ FinalExam/Node.java
@@ -0,0 +1,87 @@
+package FinalExam;
+
+public class Node {
+
+  /** Node's payload is just a string */
+  private String payload;
+  /** Pointer to next node */
+  private Node next;
+
+  /**
+   * Basic constructor
+   * 
+   * @param payload String to store in node
+   */
+  public Node(String payload) {
+    this.payload = payload;
+    this.next = null;
+  } // basic constructor
+
+  /**
+   * Set a node's next node
+   *
+   * @param next Node that this node will point to
+   */
+  public void setNext(Node node) {
+    this.next = node;
+  } // method setNext
+
+  /**
+   * Boolean about next node
+   * 
+   * @return true if this node has a next one, false otherwise
+   */
+  public boolean hasNext() {
+    return this.next != null;
+  } // method hasNext
+
+  /**
+   * Checks if a node has a specific number of nodes after it
+   *
+   * @param howMany int number of nodes this node is expected to have after it
+   */
+  public boolean hasNext(int howMany) {
+    return true;
+  } // method hasNext(int)
+
+  /**
+   * Get a node's next node.
+   * 
+   * @return the next node object, or null if none.
+   */
+  public Node getNext() {
+    return this.next;
+  } // method getNext
+
+  /**
+   * Obtain the next.next...next node from this node
+   * 
+   * @param howMany int with the number of nodes to skip
+   * @return Node after so many hops or null if end of list
+   */
+  public Node getNext(int howMany) {
+        return this;
+  } // method getNext(int)
+
+  /**
+   * Obtain a node's data content (its payload)
+   * 
+   * @return String with node's payload
+   */
+  public String getPayload() {
+    return this.payload;
+  } // method getPayload
+
+  /**
+   * String representation of the node object
+   * 
+   * @return String with information about the node
+   */
+  public String toString() {
+    String nextPayload = (this.next == null) ? "null" : this.getNext().getPayload();
+    return String.format("((%s)) --> (%s)",
+        this.payload,
+        nextPayload);
+  } // method toString
+
+}
